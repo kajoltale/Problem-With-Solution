1b236978bd1b13104dfb9b53092b74abf449ce96
kajoltale <kajoltale@gmail.com>
2018-05-29 18:12:51 +0530

    initial commit

--- RevString/RevString.java
+++ RevString/RevString.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-class RevString{
-public static void main(String args[]){
+class RevString { 
+	public static void main(String args[]) {
 
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter a string");
-String str = sc.nextLine();
-System.out.println("String is "+ str);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str = sc.nextLine();
+		System.out.println("String is "+ str);
 
-String[] splitString = str.split(" ");
-String[] revString = new String[splitString.length];
+		String[] splitString = str.split(" ");
+		String[] revString = new String[splitString.length];
 
-for (int i = 0; i < splitString.length; i++)
-{
-	revString[i] = splitString[splitString.length - i - 1];
-}
-System.out.print("Reverse string is ");
-for (int i = 0; i < splitString.length; i++)
-{
-	System.out.print(revString[i] + " ");
-}
-}
+		for (int i = 0; i < splitString.length; i++) {
+			revString[i] = splitString[splitString.length - i - 1];
+		}
+
+		System.out.print("Reverse string is ");
+		for (int i = 0; i < splitString.length; i++) {
+			System.out.print(revString[i] + " ");
+		}
+	}
 }
