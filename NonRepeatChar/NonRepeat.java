8c95c6723affc30ddcda9a7f3b02ad9dc3da5dcf
kajoltale <kajoltale@gmail.com>
2018-05-29 18:22:12 +0530

    initial commit

--- NonRepeatChar/NonRepeat.java
+++ NonRepeatChar/NonRepeat.java
@@ -1,33 +1,29 @@
 import java.util.*;
 
-class NonRepeat{
-public static void main(String args[]){
+class NonRepeat {
+	public static void main(String args[]) {
 
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-System.out.println("Enter a string");
-String str = sc.nextLine();
+		System.out.println("Enter a string");
+		String str = sc.nextLine();
 
-char c, NonRepChar;
-int count;
+		char c, NonRepChar;
+		int count;
 
-for (int i = 0; i < str.length(); i++)
-{
-	count = 0;
-	c = str.charAt(i);
-	for (int j = 0;j < str.length(); j++)
-	{
-		if (c == str.charAt(j))
-		{
-			count++;
+		for (int i = 0; i < str.length(); i++) {
+			count = 0;
+			c = str.charAt(i);
+			for (int j = 0;j < str.length(); j++) {
+				if (c == str.charAt(j)) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				NonRepChar = str.charAt(i);
+				System.out.println("First non-repeating character is " + NonRepChar);
+				break;
+			}
 		}
 	}
-	if (count == 1)
-	{
-		NonRepChar = str.charAt(i);
-		System.out.println("First non-repeating character is " + NonRepChar);
-		break;
-	}
-}
-}
 }
