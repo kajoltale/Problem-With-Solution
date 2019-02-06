2432d002a2e2698c3b2e2433e0d8ad3d3c0de474
Kajol D. Tale <kajoltale@gmail.com>
2019-02-06 12:06:14 +0530

    Update match.java

--- SubstringMatch/match.java
+++ SubstringMatch/match.java
@@ -1,33 +1,38 @@
+//Check if even length pallindrome or not.
 import java.util.*;
 
 public class HelloWorld{
 
-     public static void main(String []args){
+    public static void main(String []args){
        
-        String str = "abcabc";
+        try {
+            String str = "aacddcaaa";
         
-        String temp = "";
-        for (int i = 1; i < str.length(); i = i + 2) {
-            temp = temp + str.charAt(i-1) + str.charAt(i);
-            String result = checkSum(temp);
-            if (result.equals("No Match")) {
-                continue;
+            String temp = "";
+            for (int i = 1; i < str.length(); i = i + 2) {
+                temp = temp + str.charAt(i-1) + str.charAt(i);
+                String result = checkSum(temp);
+                if (result.equals("No Match")) {
+                    continue;
+                }
+                else {
+                    System.out.println("Result = " + result);
+                }
             }
-            else {
-                System.out.println("Result is " + result);
-            }
+        }
+        catch(Exception e) {
+            // System.out.println("Exception is" + e);
         }
     }
     
     public static String checkSum(String temp) {
         int len = temp.length()-1;
         String r = "" + temp.charAt((len/2)+1);
-        String l = "" + temp.charAt(len/2);
+        String l = "" + temp.charAt(0);
 
-        for(int i = 2; i < len; i++) {
+        for(int i = 2; i < len-1; i++) {
             r = r + temp.charAt((len/2)+i);
-            l = temp.charAt((len/2)-i+1) + l;
-            
+            l = temp.charAt(i-1) + l;
             if (l.equals(r)) {
                 return temp;
             }
